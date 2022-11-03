c08f01bb00f154cd30a4d79cea38864ded8416ef
Shohruh2000 <shertoyloqovshohruh524@gmail.com>
2022-11-03 23:04:17 +0500

    List06.py

--- List06.py
+++ List06.py
@@ -9,7 +9,7 @@ def main(list1):
     ls = []
     
     i = 0
-    l = []
+    
     
     while i < len(list1):
         if list1[i]==1:
@@ -19,5 +19,5 @@ def main(list1):
             list1[i]=[0]
             ls +=list1[i]
         i +=1
-    return l +ls
+    return ls
 print(main([1,1,0,1]))
