0663a7961446e6bcdb0affffdfd8a8a367fd090a
Shohruh2000 <shertoyloqovshohruh524@gmail.com>
2022-11-03 23:03:09 +0500

    List06.py

--- List06.py
+++ List06.py
@@ -6,4 +6,18 @@ def main(list1):
     Returns:
         list: return answer
     """
-    return
+    ls = []
+    
+    i = 0
+    l = []
+    
+    while i < len(list1):
+        if list1[i]==1:
+            list1[i]=["True"]
+            ls += list1[i]
+        else:
+            list1[i]=[0]
+            ls +=list1[i]
+        i +=1
+    return l +ls
+print(main([1,1,0,1]))
